324851c50a0600f32ff55b1ae36e4cc492f95aa9
davidepatruno <patrunodavid@gmail.com>
2018-11-20 20:35:25 +0100

    controllo numeri inseriti dall'utente work in progress

--- main.js
+++ main.js
@@ -16,28 +16,41 @@ for (var i = 0; i < 16; i++) {
 // Ora strutturiamo il prompt in cui l'utente inserirà i numeri da confrontare con quelli vietati
 var numero_utente = [];
 var vietato = false;
+var gia_inserito = false;
 var punteggio = 0;
 var contatore = 0;
 
 while (contatore < 16) {
-  var numero_utente = parseInt(prompt("inserisci un numero"));
-  numero_utente.push(numero_utente);
-  for (i = numero_utente.length; i < numero_utente.length; i++){
-
+  var prompt_utente = parseInt(prompt("inserisci un numero"));
+  numero_utente.push(prompt_utente);
+  while (gia_inserito == true) {
+    for (i = 0; i < numero_utente.lenght; i++){
+      if (prompt_utente == numero_utente[i]){
+        var gia_inserito = true;
+        console.log(prompt_utente);
+        console.log(numero_utente);
+      }
+    }
+    if (gia_inserito == true){
+      alert("Numero già inserito! inserire un altro numero");
+      // var prompt_utente = parseInt(prompt("Numero già inserito"));
+    }
   }
   punteggio++;
   contatore++;
   // document.writeln("continua così" + "<br>");
   for (i=0; i < 16; i++) {
-    if (numero_utente == numeri_vietati[i]){
+    if (prompt_utente == numeri_vietati[i]){
       var vietato = true;
       console.log(vietato);
     }
   }
   if (vietato == true){
     punteggio--;
-    contatore = 16;
+    contatore = 17;
     document.writeln("GAME OVER" + "<br>" + "hai indovinato: " + punteggio + " numeri");
-
+  }
+  if (contatore == 16) {
+    document.writeln("COMPLIMENTI" + "<br>" + "hai indovinato tutti i numeri");
   }
 }
